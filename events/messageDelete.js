5201921b9f7db6f7f60e20e69d2f71ea04b54f75
DanielaGC <danielagc676@gmail.com>
2020-01-19 16:52:38 -0300

    Added messageDelete event

---
+++ events/messageDelete.js
@@ -0,0 +1,18 @@
+const { MessageEmbed } = require("discord.js")
+module.exports = class MessageDeleteReceive {
+    constructor(client) {
+        this.client = client
+    }
+
+    run(message) {
+
+        let embed = new MessageEmbed()
+        .setColor(this.client.colors.default)
+        .setAuthor(message.author.tag, message.author.displayAvatarURL())
+        .setDescription("**Deleted text message**")
+        .addBlankField(true)
+        .addField("Message", `\`\`\`fix\n${message.content}\`\`\``)
+
+        message.guild.channels.get("468880753195745291").send(embed)
+    }
+}
