603cb6bcb4cf1c05c520ed48d15b1a8b54de9ae9
DanielaGC <42395960+DanielaGC@users.noreply.github.com>
2020-07-06 17:27:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,18 +4,3 @@ const CocoaClient = require("./src/CocoaClient.js")
 const client = new CocoaClient({ fetchAllMembers: true })
 
 client.login()
-
-// Glitch
-const http = require("http")
-const express = require("express")
-const app = express()
-
-app.get("/", (_, res) => {
-    res.sendStatus(200)
-})
-
-app.listen(process.env.PORT)
-
-setInterval(() => {
-    http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`)
-}, 15000)
